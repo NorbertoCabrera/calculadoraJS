ef024b548cf8256612c1b6abac54b6d479befc0e
Norberto <norberto.cabrera.a@gmail.com>
2021-12-23 22:00:04 -0300

    implementacion de calculos

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,14 +4,60 @@ window.addEventListener('load',()=>{
 
     const keyPadButtonsArray = Array.from(keyPadButtons);
 
-    keyPadButtonsArray.forEach((boton)=>{
+    keyPadButtonsArray.forEach((button)=>{
 
-        boton.addEventListener('click',()=>{
-            console.log(boton.innerHTML);
-        })
+        
+            button.addEventListener('click',()=>{
+                calculadora(button,display);
+            })
+
+
     });
 
 
 
 
 })
+
+
+
+const calculadora = (button,display) => {
+
+
+           switch (button.innerHTML) {
+               case 'C':
+                   borrarDisplay(display);
+                   
+                   break;
+                case '=':
+                    calcular(display);
+                    break;
+           
+               default:
+                   actualizar(display,button);
+                   break;
+           }
+
+
+
+    
+}
+
+
+const calcular = (display) =>{
+    display.innerHTML = eval(display.innerHTML);
+}
+
+
+const actualizar = (display,button) => {
+     if(display.innerHTML==0){
+        display.innerHTML='';
+     }
+      display.innerHTML = display.innerHTML+button.innerHTML; 
+}
+
+const borrarDisplay = (display) => {
+
+        display.innerHTML=0;
+
+}
